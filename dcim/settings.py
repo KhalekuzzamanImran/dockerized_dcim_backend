1514ff92af1ec8f1e80af5f5889cfd6866dcd261
Imran Khan <khalekuzzamanimran@gmail.com>
2024-09-01 16:41:38 +0600

    change csrf origins

--- dcim/settings.py
+++ dcim/settings.py
@@ -158,17 +158,13 @@ CORS_ORIGIN_ALLOW_HEADER = [
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://dev.api.eassetfswt.com',  
-    'https://dev.eassetfswt.com', 
     'http://13.215.201.211:8000',
     'http://localhost:5000',
     'http://localhost:3011',
     'http://13.214.245.15:8000',
     'http://0.0.0.0:8000',
     'http://0.0.0.0:8010',
-    'http://0.0.0.0:9000',
-    'https://awaited-mule-smiling.ngrok-free.app/auth',
-    'https://awaited-mule-smiling.ngrok-free.app'
+    'http://127.0.0.1:8010',
 ]
 
 CORS_ALLOW_HEADERS = ['*']
