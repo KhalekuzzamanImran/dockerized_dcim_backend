018452f46eebedbc28f727582071f885d0c9eaa1
Imran Khan <khalekuzzamanimran@gmail.com>
2025-02-09 14:38:48 +0600

    add trusted origin

--- dcim/settings.py
+++ dcim/settings.py
@@ -163,6 +163,7 @@ CSRF_TRUSTED_ORIGINS = [
     'http://localhost:3011',
     'http://13.214.245.15:8000',
     'http://0.0.0.0:8000',
+    'http://165.22.221.252:8000'
 ]
 
 CORS_ALLOW_HEADERS = ['*']
